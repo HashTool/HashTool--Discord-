fd869763a258f635ba31b09ec06539629df4508d
HashTool <105384466+HashTool@users.noreply.github.com>
2022-05-11 19:56:47 +0200

    Add files via upload

---
+++ hashtool.py
@@ -0,0 +1,97 @@
+import requests, os, sys, time, random, os.path, string, subprocess, pyperclip, pyautogui, urllib.request, random, threading, ctypes, base64, discord, json, datetime, shutil, aiohttp, asyncio
+from colorama import Fore, init
+from dhooks import Webhook
+from tkinter import *
+import tkinter.font as font
+from selenium import webdriver
+from PIL import Image
+from itertools import cycle
+from bs4 import BeautifulSoup
+import requests as req
+from threading import Thread as thr
+from discord.ext import commands
+from discord.ext.commands import Bot
+from plyer import notification
+from datetime import datetime
+from pypresence import Presence
+from discord import Webhook, AsyncWebhookAdapter
+from colorama import init
+from colorama import Fore, Style
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def transition():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+os.system('title HashTool /\ 2022')
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+from colorama import init, Fore, Back, Style    
+init(autoreset=True)
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ██╗░░██╗░█████╗░░██████╗██╗░░██╗  ████████╗░█████╗░░█████╗░██╗░░░░░")
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ██║░░██║██╔══██╗██╔════╝██║░░██║  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░")
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ███████║███████║╚█████╗░███████║  ░░░██║░░░██║░░██║██║░░██║██║░░░░░")
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ██╔══██║██╔══██║░╚═══██╗██╔══██║  ░░░██║░░░██║░░██║██║░░██║██║░░░░░")
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ██║░░██║██║░░██║██████╔╝██║░░██║  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗")
+print(Style.BRIGHT + Back.BLACK + Fore.CYAN + "                          ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝")
+time.sleep(2)
+
+
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                        [1] Discord Cloner                [2] Nitro Generator       [3] Discord Call Lagger (Groups)")
+print("                         ")
+print("                        [4] soon...                       [5] soon...               [6] soon...")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+
+
+global choice
+choice = input(f"""Choice: """)
+
+
+
+
+if choice == '1' or choice == '01':
+        transition()
+        script_name = 'extras/v742d/v742d.py'
+        cmd_line = [sys.executable, script_name]
+        subprocess.check_call(cmd_line)
+        main()
+
+
+
+elif choice == '2' or choice == '02':
+        transition()
+        script_name = 'extras/v743s/v743s.py'
+        cmd_line = [sys.executable, script_name]
+        subprocess.check_call(cmd_line)
+        main()
+
+
+
+elif choice == '3' or choice == '03':
+        transition()
+        script_name = 'extras/t431i/t431i.py'
+        cmd_line = [sys.executable, script_name]
+        subprocess.check_call(cmd_line)
+        main()
